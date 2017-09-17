a1ddbd4afab521c9ddc7c9b3b94195f7680f3528
river <jiang.wang2014@m2mkey.com>
2017-09-17 17:38:21 +0800

    * add function of sorting json array

--- j2t/converter.go
+++ j2t/converter.go
@@ -19,6 +19,34 @@ func JSON2HtmlTable(jsonStr string, customTitles []string) (bool, string) {
 	titles := customTitles
 	if nil == customTitles || 0 == len(titles) { // if custom titles are not provided, use json keys as titles
 		titles = getKeys(jsonArray[0])
+	} else { // if custom titles are provided, sort json array
+		for tid, title := range titles {
+			swapped := true
+			for swapped {
+				swapped = false
+				for i := 0; i < len(jsonArray)-1; i++ {
+					va, oka := jsonArray[i][title].(string)
+					vb, okb := jsonArray[i+1][title].(string)
+					if !oka || !okb {
+						swapped = false
+						break
+					}
+					if strings.Compare(va, vb) > 0 {
+						if tid != 0 {
+							va, _ := jsonArray[i][titles[tid-1]].(string)
+							vb, _ := jsonArray[i+1][titles[tid-1]].(string)
+							if va != vb {
+								continue
+							}
+						}
+						tmp := jsonArray[i]
+						jsonArray[i] = jsonArray[i+1]
+						jsonArray[i+1] = tmp
+						swapped = true
+					}
+				}
+			}
+		}
 	}
 	// convert table headers
 	if 0 == len(titles) {
